583c64d09870419e191c9ed2f98372dca81ec327
Hai Nguyen <hai427@uw.edu>
2017-01-22 21:07:50 -0800

    finished

---
+++ BigGoButton/app/src/main/java/edu/washington/hai427/biggobutton/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.washington.hai427.biggobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                count++;
+                button.setText("You have pushed me " + count + " times!");
+            }
+        });
+    }
+}
